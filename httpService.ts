f1149e5badeaf9be8c3d074cbc56d4786087f406
Vantai5869 <50700980+Vantai5869@users.noreply.github.com>
2023-03-07 17:10:59 +0700

    update

--- httpService.ts
+++ httpService.ts
@@ -1,143 +1,191 @@
-import axios, { AxiosError, AxiosResponse } from 'axios';
-import createAuthRefreshInterceptor from 'axios-auth-refresh';
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import qs from "qs";
-import { persistedStorage, removeToken, saveToken } from '../common/storage';
+
+const BASE_URL = 'https://7d83-222-254-34-56.ap.ngrok.io/api';
 
 type ApiConfig = {
-  uri: String;
+  uri: string;
   params?: Object;
   request?: any;
 };
 
-const CLIENT_BASE_URL = 'https://15ae-222-254-34-56.eu.ngrok.io/api';
-const REFRESH_TOKEN_URL = CLIENT_BASE_URL + '/refreshToken';
+export const axiosInstance: AxiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 5000,
+});
 
-class Client {
-  authAxios: any;
-  publicAxios: any;
-  constructor() {
-    this.authAxios = axios.create({
-      baseURL: CLIENT_BASE_URL,
-      headers: {
-        "Content-Type": "application/json",
-      }
-    });
+let isRefreshing = false;
+let refreshSubscribers: any[] = [];
 
-    this.publicAxios = axios.create({
-      baseURL: CLIENT_BASE_URL,
-      headers: {
-        "Content-Type": "application/json",
-      }
-    });
+const refreshAccessToken = async (): Promise<string | null> => {
+  console.log("refreshing...")
+  try {
+    const refreshToken = await AsyncStorage.getItem('refreshToken');
 
-    this.authAxios.interceptors.request.use(async (config: any) => {
-      if (!config.headers.Authorization) {
-        const token = await persistedStorage?.getItem("accessToken");
-        config.headers.Authorization = `Bearer ${token}`;
-      }
+    const response = await axiosInstance.post('/refreshToken', { refreshToken });
 
-      return config;
-    }, (error: any) => {
-      return Promise.reject(error);
-    },
-    );
+    // Save the new access token to the AsyncStorage
+    await AsyncStorage.setItem('accessToken', response.data.accessToken);
 
-    this.authAxios.interceptors.response.use(
-      (config: AxiosResponse) => {
-        console.log(config);
-        return config;
-      },
-      (error: AxiosError) => {
-        return Promise.reject(error);
-      },
-    );
+    // Resolve all the subscribers with the new access token
+    refreshSubscribers.forEach((callback) => callback(response.data.accessToken));
 
-    const refreshAuthLogic = async (failedRequest: any) => {
-      const data = {
-        refreshToken: await persistedStorage?.getItem("refreshToken")
-      };
+    // Reset the refresh flag and subscribers array
+    isRefreshing = false;
+    refreshSubscribers = [];
 
-      const options = {
-        method: 'POST',
-        data,
-        url: REFRESH_TOKEN_URL,
-      };
-
-      try {
-        const tokenRefreshResponse = await axios(options);
-        failedRequest.response.config.headers.Authorization =
-          'Bearer ' + tokenRefreshResponse.data.accessToken;
-        saveToken(tokenRefreshResponse.data);
-        return await Promise.resolve();
-      } catch (e) {
-        removeToken()
-      }
-    };
-    createAuthRefreshInterceptor(this.authAxios, refreshAuthLogic, {});
+    // Return the new access token
+    return response.data.accessToken;
+  } catch (error) {
+    // If there is an error, return null
+    return null;
   }
+};
 
+// Set the authorization header for the Axios instance
+const setAuthorizationHeader = async (config: any) => {
+  // Get the access token from the AsyncStorage
+  const accessToken = await AsyncStorage.getItem('accessToken');
+
+  // If there is an access token, set the authorization header
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+};
+
+// Add a request interceptor to the Axios instance
+axiosInstance.interceptors.request.use(
+  async (config: any) => {
+    // Set the authorization header
+    await setAuthorizationHeader(config);
+
+    // If the request is a refresh token request, return the config
+    if (config.url === '/refreshToken') {
+      return config;
+    }
+
+    // If the access token is not present, return the config
+    if (!config.headers.Authorization) {
+      return config;
+    }
+
+    // If the access token is present and the refresh flag is false, return the config
+    if (!isRefreshing) {
+      return config;
+    }
+
+    // If the access token is present and the refresh flag is true, block the request and add it to the subscribers array
+    await new Promise((resolve) => refreshSubscribers.push(resolve));
+    config.headers.Authorization = `Bearer ${await AsyncStorage.getItem('accessToken')}`;
+    return config;
+  },
+  (error: AxiosError) => {
+    // If there is an error, return the Promise.reject() method
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor to the Axios instance
+axiosInstance.interceptors.response.use(
+  (response: AxiosResponse) => {
+    // If the response is successful,
+    return response;
+  },
+  async (error: any) => {
+    // If the error is not an authentication error, return the Promise.reject() method
+    if (error.response && error.response.status !== 401) {
+      return Promise.reject(error);
+    }
+    // If the error is an authentication error and the refresh flag is false, set the refresh flag and refresh the access token
+    if (error.response && error.response.status === 401 && !isRefreshing) {
+      isRefreshing = true;
+
+      const accessToken = await refreshAccessToken();
+
+      // If the access token is refreshed, retry the original request
+      if (accessToken) {
+        const originalRequest = error.config;
+        originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+        return axiosInstance(originalRequest);
+      }
+    }
+
+    // If the error is an authentication error and the refresh flag is true, block the request and add it to the subscribers array
+    if (error.response && error.response.status === 401 && isRefreshing) {
+      return new Promise((resolve) => {
+        refreshSubscribers.push((accessToken: string) => {
+          error.config.headers.Authorization = `Bearer ${accessToken}`;
+          resolve(axiosInstance(error.config));
+        });
+      });
+    }
+
+    // If there is an error that is not related to authentication, return the Promise.reject() method
+    return Promise.reject(error);
+  }
+);
+
+
+
+ export const httpService={
   async GET(apiConfig: ApiConfig) {
-    const { uri, params } = apiConfig;
-    let url = CLIENT_BASE_URL + uri;
+    const { uri, params, ...rest } = apiConfig;
     try {
-      const res = await this.authAxios.get(url, {
+      const res = await axiosInstance.get(uri, {
         params,
+        ...rest
       });
       return res;
     } catch (error) {
-      console.log({error});
-      
       throw error;
     }
-  }
+  },
 
   async POST(apiConfig: ApiConfig) {
-    const { uri, request, params } = apiConfig;
-    let url = CLIENT_BASE_URL + uri;
-
+    const { uri, request, params,...rest } = apiConfig;
     try {
-      const res = await this.authAxios.post(url, request, {
-        params
+      const res = await axiosInstance.post(uri, request, {
+        params,
+        ...rest
       });
       return res;
     } catch (error) {
       throw error;
     }
-  }
+  },
 
   async PUT(apiConfig: ApiConfig) {
-    const { uri, request, params } = apiConfig;
-    let url = CLIENT_BASE_URL + uri;
-
+    const { uri, request, params, ...rest } = apiConfig;
     try {
-      const res = await this.authAxios.put(url, request, {
-        params
+      const res = await axiosInstance.put(uri, request, {
+        params,
+        ...rest
       });
       return res;
     } catch (error) {
       throw error;
     }
-  }
+  },
 
   async DELETE(apiConfig: ApiConfig) {
-    const { uri, params } = apiConfig;
-    let url = CLIENT_BASE_URL + uri;
-
+    const { uri, params, ...rest } = apiConfig;
     try {
-      const res = await this.authAxios.delete(url, params);
+      const res = await axiosInstance.delete(uri, {
+        params,
+        ...rest
+      });
       return res;
     } catch (error) {
       throw error;
     }
-  }
+  },
 
   async POST_ENCODED(apiConfig: ApiConfig) {
     const { uri, request, params } = apiConfig;
-    const url = CLIENT_BASE_URL + uri;
-
     const body = qs.stringify(request);
     try {
-      const res = await this.authAxios.post(url, body, {
+      const res = await axiosInstance.post(uri, body, {
         ...params,
         headers: {
           ...(params as any)?.headers,
@@ -148,15 +196,14 @@ class Client {
     } catch (error) {
       throw error;
     }
-  }
+  },
 
   async PUT_ENCODED(apiConfig: ApiConfig) {
     const { uri, request, params } = apiConfig;
-    const url = CLIENT_BASE_URL + uri;
 
     const body = qs.stringify(request);
     try {
-      const res = await this.authAxios.put(url, body, {
+      const res = await axiosInstance.put(uri, body, {
         ...params,
         headers: {
           ...(params as any)?.headers,
@@ -167,14 +214,13 @@ class Client {
     } catch (error) {
       throw error;
     }
-  }
+  },
 
   async POST_FORM_DATA(apiConfig: ApiConfig) {
     const { uri, request, params } = apiConfig;
-    const url = CLIENT_BASE_URL + uri;
 
     try {
-      const res = await this.authAxios.post(url, request, {
+      const res = await axiosInstance.post(uri, request, {
         ...params,
         headers: {
           ...(params as any)?.headers,
@@ -185,14 +231,13 @@ class Client {
     } catch (error) {
       throw error;
     }
-  }
+  },
 
   async PUT_FORM_DATA(apiConfig: ApiConfig) {
     const { uri, request, params } = apiConfig;
-    const url = CLIENT_BASE_URL + uri;
 
     try {
-      const res = await this.authAxios.put(url, request, {
+      const res = await axiosInstance.put(uri, request, {
         ...params,
         headers: {
           ...(params as any)?.headers,
@@ -204,7 +249,4 @@ class Client {
       throw error;
     }
   }
-
-};
-
-export default new Client();
+}
